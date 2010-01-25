3046c2116bbe4a753f03b61437f9d845b5c722c4
Alexis N. Mueller <alexis@muellerlex.com>
2010-01-25 02:24:46 -0500

    Added conditional inclusion for Formtastic, Vestal Versions, and utility gems to base.rb. Added Authlogic and Cancan to authentication.rb.

--- authentication.rb
+++ authentication.rb
@@ -1,7 +1,114 @@
-load_template "http://github.com/ryanb/rails-templates/raw/master/base.rb"
+require 'date'
+# Load base template
+# load_template "http://github.com/greenplastik/rails-templates/raw/master/base.rb"
+load_template "/Users/anmueller/code/rails-templates/base.rb"
 
-name = ask("What do you want a user to be called?")
-generate :nifty_authentication, name
+# Initialize variables as false
+name = ''
+authlogic = false
+cancan = false
+extra = false
+
+# ASK ALL OF THE QUESTIONS UP FRONT
+# Ask for name of user model
+name = ask("What do you want a user to be called? (Default: user)")
+
+# Ask user about Authlogic
+if yes?("Do you want to use Authlogic for authentication?")
+  authlogic = true
+end
+
+# Ask user about Authlogic extra functionality
+if authlogic
+  if yes?("Do you want the Authlogic extra functionality? (e.g.: login counts, IP adderess logging, etc.)")
+    extra = true
+  end
+end
+
+# Ask user about CanCan authorization and create initial abilities setup
+if yes?("Do you want to use CanCan for authorization?")
+  cancan = true
+end
+
+# ADD AUTHENTICATION AND, IF SELECTED, AUTHLOGIC AND CANCAN AUTHORIZATION  
+if authlogic
+  gem "authlogic", :source => "http://gemcutter.org"
+  generate("nifty_authentication #{name} --authlogic")
+else
+  generate :nifty_authentication, name
+end
+
 rake "db:migrate"
 
-git :add => ".", :commit => "-m 'adding authentication'"
+if extra
+  file "db/migrate/#{ Time.utc(*(Time.now - 100).to_a).strftime("%Y%m%d%H%M%S")}_add_extra_columns_to_#{name.tableize}.rb",
+  "class AddExtraColumnsTo#{name.tableize.titleize} < ActiveRecord::Migration
+  def self.up
+    change_table :#{name.tableize} do |t|
+    t.integer :login_count, :null => false, :default => 0
+    t.integer :failed_login_count, :null => false, :default => 0
+    t.datetime :last_request_at
+    t.datetime :current_login_at
+    t.datetime :last_login_at
+    t.string :current_login_ip
+    t.string :last_login_ip
+  end
+end
+
+def self.down
+  change_table :#{name.tableize} do |t|
+  t.remove  :login_count, :failed_login_count, :last_request_at, 
+  :current_login_at, :last_login_at, :current_login_ip, :last_login_ip
+end
+end
+end"
+end
+
+if cancan
+  gem "cancan", :source => "http://gemcutter.org"
+  file 'app/models/ability.rb', 
+
+  "class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= #{name}.new # create guest user
+    can :read, :all
+
+    case user.role.to_sym
+    when :admin
+      can :manage, :all 
+    when :moderator
+      can [:update, :read], :all
+    when :user
+      can :read, :all
+    end
+  end
+end"
+
+# Add role field to user account (creates current UTC timestamp for migration file name)
+file "db/migrate/#{ Time.utc(*(Time.now).to_a).strftime("%Y%m%d%H%M%S")}_add_role_to_#{name.tableize}.rb",
+
+"class AddRoleTo#{name.tableize.titleize} < ActiveRecord::Migration
+def self.up
+  add_column :#{name.tableize}, :role, :string, :default => 'user', :null => false
+end
+
+def self.down
+  remove_column :#{name.tableize}, :role
+end
+end"
+end
+
+# Ensure the selected gems are installed
+puts "You may need to enter your password to install the selected gems."
+rake('gems:install', :sudo => true)
+
+rake "db:migrate"
+
+git :add => "." 
+
+git :commit => "-m 'adding authentication'"
+
+# Success!
+puts "SUCCESS!"
--- base.rb
+++ base.rb
@@ -1,22 +1,113 @@
+# Initialize variables as false
+rspec = false
+formtastic = false
+vestal_versions = false
+utility = false
+
+# ASK ALL OF THE QUESTIONS UP FRONT
+# Ask user about RSpec
 if yes?("Do you want to use RSpec for testing?")
+  rspec = true
+end
+
+# Ask user about Formtastic
+if yes?("Do you want to use Formtastic for your forms?")
+  formtastic = true
+end
+
+# Ask user about Vestal Versions
+if yes?("Do you want to use Vestal Versions for model versioning?")
+  vestal_versions = true
+end
+
+# Ask user about Bluecloth and Markaby
+if yes?("Do you want to install utility gems (Bluecloth, Markaby, etc.)?")
+  utility = true
+end
+
+# ADD SELECTED GEMS
+# Add RSpec if selected
+if rspec
   plugin "rspec", :git => "git://github.com/dchelimsky/rspec.git"
   plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec-rails.git"
   generate :rspec
 end
 
+# Add Formtastic and Validation Reflection if Formtastic selected
+if formtastic
+  gem "formtastic", :source => "http://gemcutter.org"
+  gem "validation_reflection", :source => "http://gemcutter.org"
+end
+
+# Add Vestal Versions if selected
+if vestal_versions
+  gem "vestal_versions", :source => "http://gemcutter.org"
+end
+
+#Add utility gems if selected
+if utility
+  gem "bluecloth", :source => "http://gemcutter.org"
+  gem "markaby", :source => "http://gemcutter.org"
+  gem "linguistics", :source => "http://gemcutter.org"
+  gem "will_paginate", :source => "http://gemcutter.org"
+  gem "chronic", :source => "http://gemcutter.org"
+end
+
+# Add sqlite3 gem
+gem 'sqlite3-ruby', :lib => 'sqlite3'
+
+# Ensure the selected gems are installed
+puts "You may need to enter your password to install the selected gems."
+rake('gems:install', :sudo => true)
+
+# RUN GENERATORS FOR SELECTED GEMS
+# Generate Formtastic files
+if formtastic 
+  generate :formtastic
+end
+
+# Generate Vestal_Versions files
+if vestal_versions
+  generate :vestal_versions
+  rake "db:migrate"
+end
+
+# Generate default layout
 generate :nifty_layout
 
+# Initialize git repository
 git :init
 
+# Set up project files
 run "echo 'TODO add readme content' > README"
 run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
 run "cp config/database.yml config/example_database.yml"
+run "mv public/index.html public/start.html"
 
-file ".gitignore", <<-END
-log/*.log
-tmp/**/*
-config/database.yml
-db/*.sqlite3
-END
+# Delete unnecessary files
+run "rm public/favicon.ico"
+run "rm public/robots.txt"
+run "rm public/images/rails.png"
 
-git :add => ".", :commit => "-m 'initial commit'"
+# Configure .gitignore files
+run %{find . -type d -empty | grep -v "vendor" | grep -v ".git" | grep -v "tmp" | xargs -I xxx touch xxx/.gitignore}
+file '.gitignore',
+%q{
+  log/*.log
+  log/*.pid
+  db/*.db
+  db/*.sqlite3
+  db/schema.rb
+  tmp/**/*
+  .DS_Store
+  config/database.yml
+}
+
+# Add files to git repository
+git :add => "."
+
+# Initial commit
+git :commit => "-m 'Initial commit'"
+
+# Success!
+puts "SUCCESS!"
